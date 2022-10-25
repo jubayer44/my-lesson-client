8c78e01f490458f8202400924f2c805d812aa881
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-10-25 09:14:18 +0600

    login-register-pages-added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import ErrorPage from "../Pages/ErrorPage";
 import { Home } from "../Pages/Home";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
         {
             path: "/",
             element: <Home/>
+        },
+        {
+          path: "/login",
+          element: <Login/>
+        },
+        {
+          path: "/register",
+          element: <Register/>
         }
     ]
   },
